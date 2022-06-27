0b36d7372386cb9f455da6d91420f46d14b46ac7
Adam Tibbing <adam@tibbing.com>
2022-06-26 17:18:40 -0700

    Update fleet

--- src/lib/models/fleet.go
+++ src/lib/models/fleet.go
@@ -6,6 +6,7 @@ type Fleet struct {
 	Name    string
 	OwnerID string
 	Ships   []Ship
+	Course  []Position
 }
 
 // NewFleet Creates a new fleet
@@ -14,9 +15,15 @@ func NewFleet(name, ownerID string, ships []Ship) *Fleet {
 		Name:    name,
 		OwnerID: ownerID,
 		Ships:   ships,
+		Course:  []Position{},
 	}
 	result.SetRandomID()
 	return result
+}
+
+// SetCourse Sets the course for the fleet
+func (u *Fleet) SetCourse(course []Position) {
+	u.Course = course
 }
 
 // GetID Gets the ID
